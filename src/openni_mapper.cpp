948c75050d36658256e9b8d014420a7b83c6e04e
Michael K Amoako <mamoako@mit.edu>
2016-07-12 13:22:34 -0400

    The real deal

--- src/openni_mapper.cpp
+++ src/openni_mapper.cpp
@@ -10,6 +10,9 @@
 #include <vector> 
 #include <XnCodecIDs.h>
 #include <XnCppWrapper.h>
+#include <numeric>
+
+//For controlling the robot
 #include <pr2_controllers_msgs/JointTrajectoryAction.h>
 #include <actionlib/client/simple_action_client.h>
 #include <trajectory_msgs/JointTrajectoryPoint.h>
@@ -17,11 +20,11 @@
 typedef actionlib::SimpleActionClient< pr2_controllers_msgs::JointTrajectoryAction > TrajClient;
 using namespace std;
 
+//double pi = 3.14;
 double pi = 3.14159265359;
 xn::Context        g_Context;
 xn::DepthGenerator g_DepthGenerator;
 xn::UserGenerator  g_UserGenerator;
-
 XnBool g_bNeedPose   = FALSE;
 XnChar g_strPose[20] = "";
 
@@ -100,11 +103,20 @@ tf::Transform getTransform(XnUserID const& user, XnSkeletonJoint const& joint, s
     return transform; 
 }
 
-std::vector<double> publishTransforms(const std::string& frame_id) {
+std::vector<double> get_angles(const std::string& frame_id) {
     XnUserID users[15];
     XnUInt16 users_count = 15;
     g_UserGenerator.GetUsers(users, users_count);
-
+    
+    std::vector<double> all_angles (14);  // init with right arm(7), left arm(7), head(2), and base(2) [in that order] 
+    
+    //std::vector<double> r_angles (7);  //To test one arm
+    
+    //std::vector<double> l_angles (7);
+    /*
+    std::vector<double> h_angles (2);
+    std::vector<double> base_angles (2);
+    */
     for (int i = 0; i < users_count; ++i) {
         XnUserID user = users[i];
         if (!g_UserGenerator.GetSkeletonCap().IsTracking(user))
@@ -126,42 +138,96 @@ std::vector<double> publishTransforms(const std::string& frame_id) {
         tf::Transform T_cam_lelbow = getTransform(user, XN_SKEL_RIGHT_ELBOW,frame_id, "right_elbow");
         tf::Transform T_cam_lhand = getTransform(user, XN_SKEL_RIGHT_HAND, frame_id, "right_hand");
 
-        tf::Transform T_cam_rhip = getTransform(user, XN_SKEL_LEFT_HIP, frame_id, "left_hip");
-        tf::Transform T_cam_rknee = getTransform(user, XN_SKEL_LEFT_KNEE, frame_id, "left_knee");
-        tf::Transform T_cam_rfoot = getTransform(user, XN_SKEL_LEFT_FOOT, frame_id, "left_foot");
+        //tf::Transform T_cam_rhip = getTransform(user, XN_SKEL_LEFT_HIP, frame_id, "left_hip");
+        //tf::Transform T_cam_rknee = getTransform(user, XN_SKEL_LEFT_KNEE, frame_id, "left_knee");
+        //tf::Transform T_cam_rfoot = getTransform(user, XN_SKEL_LEFT_FOOT, frame_id, "left_foot");
 
-        tf::Transform T_cam_lhip = getTransform(user, XN_SKEL_RIGHT_HIP, frame_id, "right_hip");
-        tf::Transform T_cam_lknee = getTransform(user, XN_SKEL_RIGHT_KNEE, frame_id, "right_knee");
-        tf::Transform T_cam_lfoot = getTransform(user, XN_SKEL_RIGHT_FOOT, frame_id, "right_foot");
+        //tf::Transform T_cam_lhip = getTransform(user, XN_SKEL_RIGHT_HIP, frame_id, "right_hip");
+        //tf::Transform T_cam_lknee = getTransform(user, XN_SKEL_RIGHT_KNEE, frame_id, "right_knee");
+        //tf::Transform T_cam_lfoot = getTransform(user, XN_SKEL_RIGHT_FOOT, frame_id, "right_foot");
 
-	tf::Transform T_torso_shoulder = T_cam_torso.inverse() * T_cam_rshoulder;
-	tf::Transform T_shoulder_elbow = T_cam_rshoulder.inverse() * T_cam_relbow;
-	tf::Quaternion q1 = T_torso_shoulder.getRotation();
+
+
+	
+
+	
+	tf::Transform T_torso_rshoulder = T_cam_torso.inverse() * T_cam_rshoulder;
+	//tf::Transform T_torso_rshoulder = T_cam_rhand.inverse() * T_cam_rshoulder; //The hand frame is fixed while the torso frame is not. If we want to include base-movement we can adjust for this. 
+	tf::Transform T_shoulder_relbow = T_cam_rshoulder.inverse() * T_cam_relbow;
+	tf::Quaternion q1 = T_torso_rshoulder.getRotation();
 	tf::Matrix3x3 m1(q1);
-	double roll, pitch, yaw;
-	m1.getRPY(roll, pitch, yaw);
-	tf::Quaternion q2 = T_shoulder_elbow.getRotation();
+	double rroll, rpitch, ryaw;
+	m1.getRPY(rroll, rpitch, ryaw);
+	tf::Quaternion q2 = T_shoulder_relbow.getRotation();
 	tf::Matrix3x3 m2(q2);
 	double x, r_elbow_angle, z;
 	m2.getRPY(x, r_elbow_angle, z);
-	double r_roll_angle = roll - pi/2;
-	double r_pan_angle = pitch - pi/2;
-	double r_lift_angle = -1 * yaw;
+	double r_roll_angle = rroll - pi/2;
+	double r_pan_angle = rpitch - pi/2;
+	double r_lift_angle = -1 * ryaw;
+
+	tf::Transform T_torso_lshoulder = T_cam_torso.inverse() * T_cam_lshoulder;
+	tf::Transform T_shoulder_lelbow = T_cam_lshoulder.inverse() * T_cam_lelbow;
+	tf::Quaternion q3 = T_torso_lshoulder.getRotation();
+	tf::Matrix3x3 m3(q3);
+	double lroll, lpitch, lyaw;
+	m3.getRPY(lroll, lpitch, lyaw);
+	tf::Quaternion q4 = T_shoulder_lelbow.getRotation();
+	tf::Matrix3x3 m4(q4);
+	double x2, l_elbow_angle, z2;
+	m4.getRPY(x2, l_elbow_angle, z2);
+	double l_roll_angle = pi/2 - lroll;
+	double l_pan_angle = pi/2 + lpitch;
+	double l_lift_angle = lyaw;
+
 
 	//std::cout << "Right-Arm Roll: " << r_roll_angle << ", Right-Arm Pan: " << r_pan_angle << ", Right-Arm Lift: " << r_lift_angle << std::endl;
+	
    	if (q2[1] < 0.7)
 		r_elbow_angle = pi - r_elbow_angle;
+
+	
+   	if (q1[1] > 0.53)
+		r_pan_angle = -1 * r_pan_angle;
+		//r_roll_angle = r_roll_angle + pi;
+    
+    
+		
+	//if (q1[0] < 0.03)
+	//	r_roll_angle = r_roll_angle + pi;
+	
+	
+		//r_roll_angle = r_roll_angle + pi;
+	//std::cout << "Elbow Quaternion: " << q2[1] << std::endl;
+	//std::cout << "L-Elbow Quaternion: " << q4[1] << std::endl;
 	//std::cout << "Elbow Angle: " << r_elbow_angle/(pi) * 180 << std::endl;
-       
-	std::vector<double> angles;
-	angles.push_back(r_pan_angle);
-	angles.push_back(max(r_lift_angle, -0.53));
-	angles.push_back(r_roll_angle);
-	angles.push_back(max((r_elbow_angle - pi), -2.0));
-	angles.push_back(0);
-	angles.push_back(0);
-	angles.push_back(0);
-	return angles; 
+	//std::cout << "L-Elbow Angle: " << l_elbow_angle/(pi) * 180  << std::endl;
+        
+	all_angles[0] = min(max(r_pan_angle, -2.28), 0.68);
+
+    all_angles[1] = min(max(r_lift_angle, -0.53), 1.3);
+
+	all_angles[2] = r_roll_angle;	
+
+    all_angles[3] = max((r_elbow_angle - pi), -2.0);
+
+	
+	all_angles[7] = l_pan_angle;
+	all_angles[8] = min(max(r_lift_angle, -0.53), 1.3);
+	//all_angles[9] = l_roll_angle;
+	//all_angles[10] = l_elbow_angle;
+
+	
+	//std::cout << "Right Roll Quaternion: " << q1[0] << std::endl;
+	//std::cout << "Right Lift Quaternion: " << q1[2] << std::endl;
+	//std::cout << "Right Pan Quaternion: " << q1[1] << std::endl;
+	//std::cout << "Right Pan Angle: " << all_angles[0] << std::endl;
+	//std::cout << "Right-Arm Roll: " << r_roll_angle << std::endl;
+
+
+	return all_angles;
+        //return {r_angles, l_angles, h_angles, base_angles};
+
 
 	
     }
@@ -173,7 +239,6 @@ std::vector<double> publishTransforms(const std::string& frame_id) {
 		ROS_ERROR("%s failed: %s", what, xnGetStatusString(nRetVal));\
 		return nRetVal;												\
 	}
-
 
 
 int main(int argc, char **argv) {
@@ -226,16 +291,20 @@ int main(int argc, char **argv) {
 	nRetVal = g_Context.StartGeneratingAll();
 	CHECK_RC(nRetVal, "StartGenerating");
 
-	ros::Rate r(30);
+	ros::Rate r(10);
 
-        
+
         ros::NodeHandle pnh("~");
         string frame_id("camera_depth_frame");
         pnh.getParam("camera_frame_id", frame_id);
 	
-	TrajClient* traj_client_;
-	traj_client_ = new TrajClient("r_arm_controller/joint_trajectory_action", true);
+	TrajClient* r_traj_client_;
+	r_traj_client_ = new TrajClient("r_arm_controller/joint_trajectory_action", true);
+	TrajClient* l_traj_client_;
+	l_traj_client_ = new TrajClient("l_arm_controller/joint_trajectory_action", true);	
+
     	pr2_controllers_msgs::JointTrajectoryGoal r_goal;
+    	r_goal.trajectory.header.stamp = ros::Time::now();
    	r_goal.trajectory.joint_names.push_back("r_shoulder_pan_joint");
     	r_goal.trajectory.joint_names.push_back("r_shoulder_lift_joint");
     	r_goal.trajectory.joint_names.push_back("r_upper_arm_roll_joint");
@@ -244,17 +313,132 @@ int main(int argc, char **argv) {
     	r_goal.trajectory.joint_names.push_back("r_wrist_flex_joint");
     	r_goal.trajectory.joint_names.push_back("r_wrist_roll_joint");
 
+    	pr2_controllers_msgs::JointTrajectoryGoal l_goal;
+    	l_goal.trajectory.header.stamp = ros::Time::now();
+   	l_goal.trajectory.joint_names.push_back("l_shoulder_pan_joint");
+    	l_goal.trajectory.joint_names.push_back("l_shoulder_lift_joint");
+    	l_goal.trajectory.joint_names.push_back("l_upper_arm_roll_joint");
+    	l_goal.trajectory.joint_names.push_back("l_elbow_flex_joint");
+    	l_goal.trajectory.joint_names.push_back("l_forearm_roll_joint");
+    	l_goal.trajectory.joint_names.push_back("l_wrist_flex_joint");
+    	l_goal.trajectory.joint_names.push_back("l_wrist_roll_joint");
+
+	std::cout << "Please go to PSI Pose" << std::endl;
+    trajectory_msgs::JointTrajectoryPoint sr_point;
+    trajectory_msgs::JointTrajectoryPoint sl_point;
+	std::vector<double> sr_angles(7);
+	std::vector<double> sl_angles(7);
+	sr_point.positions = sr_angles;
+	sl_point.positions = sl_angles;
+	sr_point.positions[0] = -pi/2;
+	sr_point.positions[1] = 0.0;
+	sr_point.positions[2] = 0.0;
+	sr_point.positions[3] = -pi/2;
+	sr_point.positions[4] = 0.0;
+	sr_point.positions[5] = 0.0;
+	sr_point.positions[6] = 0.0;
+	sl_point.positions[0] = pi/2;
+	sl_point.positions[1] = 0.0;
+	sl_point.positions[2] = 0.0;
+	sl_point.positions[3] = -pi/2;
+	sl_point.positions[4] = 0.0;
+	sl_point.positions[5] = 0.0;
+	sl_point.positions[6] = 0.0;
+	sr_point.time_from_start = ros::Duration(5.0);
+	r_goal.trajectory.points.push_back(sr_point);
+    sl_point.time_from_start = ros::Duration(5.0);
+	l_goal.trajectory.points.push_back(sl_point);
+	r_traj_client_->sendGoal(r_goal); 
+	l_traj_client_->sendGoal(l_goal);
+
+	double rprev_val_1 = -pi/2, rprev_val_2 = 0.0, rprev_val_3 = 0.0, rprev_val_4 = -pi/2; 
+	//double lprev_val_1 = pi/2, lprev_val_2 = 0.0, lprev_val_3 = 0.0, lprev_val_4 = -pi/2; 
+	//double rprev_val_1;
+	//double val = 5.0; 
+	int i_r1 = 0;
+	double r1_avg = -pi/2;
+	std::vector<double> r1_avg_vals (7, -pi/2); 
+	while (ros::ok()) 
+	{
+		g_Context.WaitAndUpdateAll();
+		std::vector<double> angles = get_angles(frame_id);		
+		trajectory_msgs::JointTrajectoryPoint r_point;
+    		trajectory_msgs::JointTrajectoryPoint l_point;
+		/*
+		if (i > 0)
+		{
+			val = 0.05; 
+
+		}
+		if (abs(angles.at(1) - rprev_val_1) > val)
+		{
+		*/
+	    std::vector<double> r_angles(7);
+	    std::vector<double> l_angles(7);
+	    r_point.positions = r_angles;
+	    l_point.positions = l_angles;
+	    //r1_avg_vals[i_r1] = angles.at(0);
+	    //r1_avg = (std::accumulate(r1_avg_vals.begin(), r1_avg_vals.end(), 0.0)) / 3.0;
+	    //r_point.positions[0] = min(angles.at(0), max(abs(angles.at(0)), 0.0));
+	
+	    r_point.positions[0] = angles.at(0);
+	    //r_point.positions[0] = r1_avg;		
+        r_point.positions[1] = angles.at(1);
+        r_point.positions[2] = angles.at(2);		
+        r_point.positions[3] = angles.at(3);
+	    //r_point.positions[1] = min(angles.at(1), max(abs(angles.at(1)), 0.0));
+	    //r_point.positions[2] = min(angles.at(2), max(abs(angles.at(2)), 0.0));
+	    //r_point.positions[3] = min(angles.at(3), max(abs(angles.at(3)), 0.0));
+	    rprev_val_1 = r_point.positions[0];
+	    rprev_val_2 = r_point.positions[1];
+	    rprev_val_3 = r_point.positions[2];
+	    rprev_val_4 = r_point.positions[3];
+	    l_point.positions[0] = angles.at(7);
+	    l_point.positions[1] = angles.at(8);
+	    l_point.positions[2] = angles.at(9);
+	    l_point.positions[3] = angles.at(10);
+    	r_point.time_from_start = ros::Duration(1);
+	    r_goal.trajectory.points.push_back(r_point);
+        l_point.time_from_start = ros::Duration(0.3);
+	    l_goal.trajectory.points.push_back(l_point);
+	    //std::cout << r_point.positions[2] << std::endl;
+	    r_traj_client_->sendGoal(r_goal); 
+	    //l_traj_client_->sendGoal(l_goal); 
+		
+		
+		
+		if (i_r1 = 6)
+		{
+			i_r1 = 0;
+		}
+        i_r1 +=1;
+        
+		r.sleep();
+				
+		
+
+	
+	}
+	
+	/*
+	int i = 0;
 
 	while (ros::ok()) {
+		i++; 
 		g_Context.WaitAndUpdateAll();
-    		trajectory_msgs::JointTrajectoryPoint point;
-		point.positions = publishTransforms(frame_id);
-	    	point.time_from_start = ros::Duration(0.5);
-		r_goal.trajectory.points.push_back(point);
-		//traj_client_->sendGoal(r_goal); 
+    		trajectory_msgs::JointTrajectoryPoint r_point;
+    		trajectory_msgs::JointTrajectoryPoint l_point;
+		r_point.positions = get_angles(frame_id);
+		//r_point.positions = get_angles(frame_id)[0];
+	    	r_point.time_from_start = ros::Duration(1.0);
+		r_goal.trajectory.points.push_back(r_point);
+		r_traj_client_->sendGoal(r_goal); 
+		//std::cout << "Moving" << std::endl;
 		r.sleep();
 	}
-	delete traj_client_; 
+	*/
+	delete r_traj_client_; 
+	delete l_traj_client_; 
 	g_Context.Shutdown();
 	return 0;
 }
